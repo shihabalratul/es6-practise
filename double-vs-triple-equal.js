912c81d6d38e45a46e1b715ed71e076c0662f553
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-03 16:21:48 +0100

    double vs triple testes

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,15 @@
+const first = 1;
+const second = true;
+
+if(first==second){
+	console.log("conditon is true");
+}
+
+// if(first===second){
+// 	console.log("condition is true");
+// }
+else{
+	console.log("condition is false");
+}
+
+
