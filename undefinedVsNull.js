e0b8fa2c304556881e4862f93c0a17eb31cf7cd2
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-03 15:49:37 +0100

    null vs undefined tested

---
+++ undefinedVsNull.js
@@ -0,0 +1,16 @@
+// let bird;
+// console.log(bird);
+
+// function add(num1, num2){
+// 	console.log(num1, num2)
+// 	return num1, num2;
+// }
+
+// const boy = {name:"cool dude", number:1234567};
+// console.log(boy.school);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 4, 6];
+console.log(ages[10]);
