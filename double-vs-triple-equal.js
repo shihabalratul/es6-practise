65a6bfaf95e9b36d5c0865379a28ea0d9def8e3d
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-03 16:34:18 +0100

    tested with triple equal

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,15 +1,22 @@
-const first = 1;
-const second = true;
+// const first = 1;
+// const second = true;
 
-if(first==second){
-	console.log("conditon is true");
-}
+// if(first==second){
+// 	console.log("conditon is true");
+// }
 
 // if(first===second){
 // 	console.log("condition is true");
 // }
-else{
-	console.log("condition is false");
-}
+// else{
+// 	console.log("condition is false");
+// }
 
+
+if(`2`===2){
+	console.log("inside if");
+}
+else{
+	console.log("inside else");
+}
 
